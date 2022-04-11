85bdda36b846a09d39b796a6665254ba77057618
Splash-07 <59502884+Splash-07@users.noreply.github.com>
2022-04-11 10:42:57 +0300

    init

--- src/components/Tree/TreeStage.tsx
+++ src/components/Tree/TreeStage.tsx
@@ -1,4 +1,5 @@
-import { Sprite, Stage, TilingSprite } from "@inlet/react-pixi";
+import { Sprite, Stage, Container, TilingSprite, Graphics } from "@inlet/react-pixi";
+import { useState } from "react";
 import { parseTreeData } from "../../lib/parseTreeData";
 import Tree from "./Tree";
 
@@ -9,28 +10,35 @@ import TreeViewport from "./TreeViewport";
 const TreeStage = () => {
   const { nodes, constants, connectionMap } = parseTreeData();
 
+  const [stageOptions, setStageOptions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
   const worldOptions = {
     minScale: constants.imageZoomLevels[0], // (max zoomOut)
     maxScale: constants.imageZoomLevels[3], // (max zoomIn)
     posX: (constants.minX + constants.maxX) / 2,
     posY: (constants.minY + constants.maxY) / 4.95,
-    windowWidth: window.innerWidth,
-    windowHeight: window.innerHeight - 0.1,
   };
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight} options={{ antialias: true }}>
+    <Stage width={stageOptions.width} height={stageOptions.height} options={{ antialias: true }}>
       <TreeContainerFilter
         adjust={{ saturation: 1, brightness: 1, gamma: 1, contrast: 0.95 }}
         blur={{ blur: 0.1, quality: 1, pixelSize: [0.1, 0.1] }}
       >
         <TilingSprite
           image="./Background2.png"
-          width={window.innerWidth}
-          height={window.innerHeight}
+          width={stageOptions.width}
+          height={stageOptions.height}
           tilePosition={{ x: 0, y: 0 }}
         />
-        <TreeViewport constants={constants} worldOptions={worldOptions}>
+        <TreeViewport
+          constants={constants}
+          width={stageOptions.width}
+          height={stageOptions.height}
+          worldOptions={worldOptions}
+        >
           <TreeBgFilter blur={{ blur: 0.15 }}>
             <Sprite
               image="./AtlasPassiveBackground.png"
